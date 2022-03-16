549624dcdee991630b1f7e2ee1e60de33a1faade
alberto Monteiro <amp35br@hotmail.com>
2022-03-16 20:29:07 -0300

    models

--- atendimento/models.py
+++ atendimento/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class assunto(models.Model):
+    assunto = models.CharField(max_length=150)
+
+
+class publico(models.Model):
+    publico = models.CharField(max_length=50)
+    nome = models.CharField(max_length=150)
+
+
+class tipo(models.Model):
+    tipo = models.CharField(max_length=50)
