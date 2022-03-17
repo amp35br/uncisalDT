19a4e26de5349a14b98041619bc783a094185080
alberto Monteiro <amp35br@hotmail.com>
2022-03-16 23:13:42 -0300

    Models_prontos

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from atendimento.models import publico
+from atendimento.models import assunto, atender, publico, tipo
 
 admin.site.register(publico)
+admin.site.register(tipo)
+admin.site.register(assunto)
+admin.site.register(atender)
--- atendimento/models.py
+++ atendimento/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 
 
+class atender(models.Model):
+    atende = models.DateTimeField(auto_now_add=True)
+    publico = models.ForeignKey("publico", on_delete=models.CASCADE, null=True)
+    tipo = models.ForeignKey("tipo", on_delete=models.CASCADE, null=True)
+    assunto = models.ForeignKey("assunto", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return str(self.atende)
+
+
 class assunto(models.Model):
     assunto = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.assunto
 
 
 class publico(models.Model):
     publico = models.CharField(max_length=50)
-    nome = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.publico
 
 
 class tipo(models.Model):
     tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tipo
